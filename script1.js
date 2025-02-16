4c2cbefa2a93622a290fc10ef0a3b83ea37bd3af
131MohitYadav <yadavjimohit54@gmail.com>
2025-02-16 16:34:35 +0530

    16 Feb 2025

--- script1.js
+++ script1.js
@@ -310,3 +310,32 @@ submitBtn.onclick = saveHighscore;
 // Start quiz after clicking start quiz
 
 startBtn.onclick = quizStart;
+
+
+// // LOGIN AND SIGN UP AUTHENTICATION JS
+
+// const sign = () => {
+//     let userdata = {
+//         name: document.querySelector('#name').value,
+//         email: document.querySelector('#email').value,
+//         password: document.querySelector('#password').value
+//     }
+//     localStorage.setItem("userdata",JSON.stringify(userdata))
+
+
+// }
+
+// let data = JSON.parse(localStorage.getItem('userdata'))
+// console.log(data)
+
+// function log(){
+//     let logindata = {
+//         loginemail: document.querySelector('#loginemail').value,
+//         loginpassword:document.querySelector('#loginpass').value,
+//     }
+//     if ( data.email != logindata.loginemail || data.password != logindata.loginpassword){
+//         alert("User not Found")
+//         return false;
+//     }
+
+// }
