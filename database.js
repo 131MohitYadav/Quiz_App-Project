d0bddba26694668c6dc2bbfcb779accaec0b2686
131MohitYadav <yadavjimohit54@gmail.com>
2025-02-28 02:48:27 +0530

    27 Feb 2025

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
+// Fetch and display data from the API
 async function fetdata() {
-
-    let data = await fetch('http://localhost:3000/student')
+    let data = await fetch('http://localhost:3000/student');
     let fdata = await data.json();
 
     let tdata = fdata.map((e) => `
@@ -10,92 +10,117 @@ async function fetdata() {
         <td> ${e.contact}</td>
         <td> ${e.age}</td>
         <td> ${e.city}</td>
-         <td> ${e.test}</td>
+        <td> ${e.test}</td>
         <td> ${e.marks}</td>
-        
-        <td><button onclick= "mydel( '${e.id}') "> Delete </button> </td>
-        <td><button onclick= "myedit( '${e.id}') "> EDIT </button> </td>
- 
-        </tr>
-    `).join("")
+        <td> ${e.result}</td>
+        <td><button onclick="mydel('${e.id}')"> Delete </button></td>
+        <td><button onclick="myedit('${e.id}')"> EDIT </button></td>
+    </tr>
+    `).join("");
 
-    document.querySelector('#display').innerHTML = tdata
-
+    document.querySelector('#display').innerHTML = tdata;
 }
 
-fetdata()
+// Call function to load data
+fetdata();
 
+// Function to delete data
 function mydel(id) {
-
-    fetch(`http://localhost:3000/student/${id}`, {
-
-        method: 'DELETE'
-    }
-    )
-        .then(r => alert("delete successfully..!!"))
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "No, cancel!",
+        allowOutsideClick: false,
+    }).then((result) => {
+        if (result.isConfirmed) {
+            fetch(`http://localhost:3000/student/${id}`, {
+                method: 'DELETE'
+            })
+            .then(() => {
+                Swal.fire("Deleted!", "The student record has been deleted.", "success");
+                fetdata(); // Refresh data
+            });
+        }
+    });
 }
 
-
+// Function to insert new data
 function insertdata() {
-
     let frmdata = {
+        name: document.querySelector('#name').value,
+        age: document.querySelector('#age').value,
+        contact: document.querySelector('#contact').value,
+        city: document.querySelector('#city').value,
+        test: document.querySelector('#test').value,
+        marks: document.querySelector('#marks').value,
+        result: document.querySelector('#result').value
+    };
 
-        name:document.querySelector('#name').value,
-        age:document.querySelector('#age').value,
-        contact:document.querySelector('#contact').value,
-        city:document.querySelector('#city').value,
-        test:document.querySelector('#test').value,
-        marks:document.querySelector('#marks').value
-    }
-
-    fetch ('http://localhost:3000/student' , {
-        method:'POST', 
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body:JSON.stringify(frmdata)
+    fetch('http://localhost:3000/student', {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(frmdata)
     })
-    .then(r=>alert ("data insert sucsses"))
+    .then(() => {
+        Swal.fire("Success!", "Student data inserted successfully!", "success");
+        fetdata(); // Refresh data
+    });
 }
 
+// Function to edit data
 async function myedit(id) {
-
-    let edata = await fetch(`http://localhost:3000/student/${id}`)
-    let fdata = await edata.json()
+    let edata = await fetch(`http://localhost:3000/student/${id}`);
+    let fdata = await edata.json();
 
     let frm = `
-    <input type="text" value=" ${fdata.name}" id="name1"> <br><br>
-    <input type="text" value=" ${fdata.age}" id="age1"> <br><br>
-    <input type="text" value=" ${fdata.contact}" id="contact1"> <br><br>
-    <input type="text" value=" ${fdata.city}" id="city1"> <br><br>
-        <input type="text" value=" ${fdata.test}" id="test1"> <br><br>
-    <input type="text" value=" ${fdata.marks}" id="marks1"> <br><br>
+    <input type="text" value="${fdata.name}" id="name1"><br><br>
+    <input type="text" value="${fdata.age}" id="age1"><br><br>
+    <input type="text" value="${fdata.contact}" id="contact1"><br><br>
+    <input type="text" value="${fdata.city}" id="city1"><br><br>
+    <input type="text" value="${fdata.test}" id="test1"><br><br>
+    <input type="text" value="${fdata.marks}" id="marks1"><br><br>
+    <input type="text" value="${fdata.result}" id="result1"><br><br>
+    <button onclick="finalupdate('${fdata.id}')">Update</button>
+    `;
 
-
-    <input type="submit" onclick = "finalupdate('${fdata.id}')">
-    `
-   document.querySelector('#showedit').innerHTML =frm ;
+    document.querySelector('#showedit').innerHTML = frm;
 }
 
+// Function to update data
 function finalupdate(id) {
-
     let final = {
+        name: document.querySelector('#name1').value,
+        age: document.querySelector('#age1').value,
+        contact: document.querySelector('#contact1').value,
+        city: document.querySelector('#city1').value,
+        test: document.querySelector('#test1').value,
+        marks: document.querySelector('#marks1').value,
+        result: document.querySelector('#result1').value
+    };
 
-        name:document.querySelector('#name1').value,
-        age:document.querySelector('#age1').value,
-        contact:document.querySelector('#contact1').value,
-        city:document.querySelector('#city1').value,
-        test:document.querySelector('#test1').value,
-        marks:document.querySelector('#marks1').value
-    }
-
-    fetch(`http://localhost:3000/student/${id}`, {
-  
-        method:'PUT', 
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body:JSON.stringify(final)
-    })
-    .then(r=>alert ("data update sucsses"))
+    Swal.fire({
+        title: "Do you want to save the changes?",
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Save",
+        denyButtonText: `Don't save`,
+        allowOutsideClick: false,
+    }).then((result) => {
+        if (result.isConfirmed) {
+            fetch(`http://localhost:3000/student/${id}`, {
+                method: 'PUT',
+                headers: { 'Content-type': 'application/json' },
+                body: JSON.stringify(final)
+            })
+            .then(() => {
+                Swal.fire("Saved!", "Your changes have been saved.", "success");
+                fetdata(); // Refresh data
+            });
+        } else if (result.isDenied) {
+            Swal.fire("Changes are not saved", "", "info");
+        }
+    });
 }
