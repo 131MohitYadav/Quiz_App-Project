aee9388033d3fdd482a6a5a5fcff42b700c59005
131MohitYadav <yadavjimohit54@gmail.com>
2025-03-01 01:25:07 +0530

    1 March 2025

--- form.js
+++ form.js
@@ -43,3 +43,21 @@ function log() {
         return false;
     }
 }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const passwordInput = document.getElementById("password");
+    const eyeIcon = document.getElementById("eye");
+
+    eyeIcon.addEventListener("click", function () {
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+            eyeIcon.classList.remove("fa-eye");
+            eyeIcon.classList.add("fa-eye-slash");
+        } else {
+            passwordInput.type = "password";
+            eyeIcon.classList.remove("fa-eye-slash");
+            eyeIcon.classList.add("fa-eye");
+        }
+    });
+});
