3a0744e3ce04e24038dfb8cbd4a146b1014adec3
131MohitYadav <yadavjimohit54@gmail.com>
2026-07-08 22:06:05 +0530

    8 july 26

--- Profile.js
+++ Profile.js
@@ -5,5 +5,14 @@ document.getElementById("progressText").innerHTML = progress + "%";
 
 function editProfile(){
 
-    let neName
+    let neName = prompt("Enter Your Name");
+    if ( newName != null && newName != ""){
+        document.getElementById("username").innerHTML = newName;
+    }
+
+    let newEmail = prompt("Enter Your Email");
+    if ( newEmail != null && newEmail != ""){
+        document.getElementById("email").innerHTML = newEmail;
+    }
+    alert("Profile Updated Successfully");
 }
