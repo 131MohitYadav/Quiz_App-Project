7212d246608caf921a13c8af9dcdc7b65545fe82
131MohitYadav <yadavjimohit54@gmail.com>
2026-07-22 21:31:20 +0530

    22 july 26

--- form.js
+++ form.js
@@ -64,3 +64,38 @@ function log() {
 }
 
 // Password toggle for sign up page
+document.addEventListener("DOMContentLoaded", function() {
+    const passwordInput = document.getElementById("password");
+    const eyeIcon = document.getElementById("eye");
+
+
+    if ( passwordInput && eyeIcon) {
+        eyeIcon.addEventListener("click", function() {
+            if (passwordInput.type === "password"){
+                passwordInput.type = "text";
+                eyeIcon.classList.remove("fa-eye");
+                eyeIcon.classList.add("fa-eye-slash");
+            }
+            else{
+                passwordInput.type = "password";
+                eyeIcon.classList.remove("fa-eye-slash");
+                eyeIcon.classList.add("fa-eye");
+            }
+        });
+    }
+});
+
+// password toggle for login page
+
+document.addEventListener("DOMContentLoaded", function () {
+    const passwordInput = document.getElementById("loginpass");
+    const eyeIcon = document.getElementById("eye");
+
+    if (passwordInput && eyeIcon){
+        eyeIcon.addEventListener("click", function() {
+            if ( passwordInput.type === "password"){
+                passwordInput.type = "text"
+            }
+        })
+    }
+})
