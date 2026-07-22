817bfcc1568e176537483694df50f8c38f1b0367
131MohitYadav <yadavjimohit54@gmail.com>
2026-07-22 21:33:16 +0530

    22 july 26

--- form.js
+++ form.js
@@ -94,8 +94,15 @@ document.addEventListener("DOMContentLoaded", function () {
     if (passwordInput && eyeIcon){
         eyeIcon.addEventListener("click", function() {
             if ( passwordInput.type === "password"){
-                passwordInput.type = "text"
+                passwordInput.type = "text";
+                eyeIcon.classList.remove("fa-eye");
+                eyeIcon.classList.add("fa-eye-slash");
             }
-        })
+            else{
+                passwordInput.type = "password";
+                eyeIcon.classList.remove("fa-eye-slash");
+                eyeIcon.classList.add("fa-eye");
+            }
+        });
     }
-})
+});
